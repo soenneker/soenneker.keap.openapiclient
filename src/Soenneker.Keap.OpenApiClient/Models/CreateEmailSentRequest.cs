43c105f154d8954dc1bee9baa7cb79b3221e88e0
Jake Soenneker <jake@soenneker.com>
2026-08-24 17:26:00 +0000

    Automated update

--- src/Soenneker.Keap.OpenApiClient/Models/CreateEmailSentRequest.cs
+++ src/Soenneker.Keap.OpenApiClient/Models/CreateEmailSentRequest.cs
@@ -8,7 +8,7 @@ using System;
 namespace Soenneker.Keap.OpenApiClient.Models
 {
     /// <summary>
-    /// Request to create an email record
+    /// The email request that failed
     /// </summary>
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     public partial class CreateEmailSentRequest : IAdditionalDataHolder, IParsable
